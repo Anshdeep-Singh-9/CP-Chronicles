1e3a36da71d2e590f26597859be696b501698308
Lavay Garg <garglavay@gmail.com>
2025-12-26 18:26:55 +0530

    added essentials in Solution1.cpp

--- Problems/Mathematics/Day-01/sol/Lavay Garg/Solution1.cpp
+++ Problems/Mathematics/Day-01/sol/Lavay Garg/Solution1.cpp
@@ -1,3 +1,39 @@
+/*# PROBLEM STATEMENT :
+
+There are **n people** standing in a circle. They are numbered from **1 to n** in a clockwise order. The circle is **even** (i.e. *n* is even).
+
+Each person is looking at the person standing exactly opposite him in the circle.
+
+You are given three **distinct** integers **a**, **b**, and **c**. It is known that person **a** is looking at person **b**.
+
+Determine the number of the person that person **c** is looking at.  
+If there are **multiple answers**, print **any** of them.  
+If there is **no answer**, print **-1**.*/
+/*## Example
+
+### Input
+```
+7
+6 2 4
+2 3 1
+2 4 10
+5 3 4
+1 3 2
+2 5 4
+4 3 2
+```
+
+
+### Output
+```
+8
+-1
+-1
+-1
+4
+1
+-1
+```*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -22,3 +58,5 @@ int main() {
         }
     }
 }
+//Time complexity =O(t)
+//Space complexity=O(1)
